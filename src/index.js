41098e0e4ea2832b11a4451ed47824ec97335fd8
zefiguefool <zefiguefool@gmail.com>
2022-04-14 21:06:35 +0200

    passsage a 14 aqualizeur et retour à 1 seul

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.scss';
 
 ( () => {
 let canvas,canvas2, firstCanvas, secondCanvas,
-    ctx,ctx2,ctx3,ctx4,ctx5,ctx6,ctx7,ctx8,
+    ctx, ctx2, ctx3, ctx4, ctx5, ctx6, ctx7, ctx8, ctx9, ctx10, ctx11, ctx12, ctx13, ctx14, ctx15,
     source,
     context,
     analyser,
@@ -12,9 +12,9 @@ let canvas,canvas2, firstCanvas, secondCanvas,
     bar_width,
     bar_height;
 
-let audio = new Audio("/media/do-i-wanna-know.mp3");
+let audio = new Audio("./media/do-i-wanna-know.mp3");
 
-    audio.src = "/media/do-i-wanna-know.mp3";
+    audio.src = "./media/do-i-wanna-know.mp3";
     audio.controls = true;
     audio.loop = false;
     audio.autoplay = false;
@@ -101,21 +101,8 @@ let FrameLooper = () => {
     fbc_array = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(fbc_array);
     if(audio.currentTime < 8){
-        bar_count = window.innerWidth / 8;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
-        ctx.fillStyle = "#ffffff";
-        ctx2.fillStyle = "#ffffff";
-        for (var i = 0; i < bar_count; i++) {
-            bar_pos = i * 16;
-            bar_width = 4;
-            bar_height = -(fbc_array[i] / 2);
-    
-            ctx.fillRect(bar_pos, canvas.height, bar_width, bar_height);
-            ctx2.fillRect(bar_pos, canvas2.height, bar_width, bar_height);
-        }
+        modifySampleEqualizerCanvas(ctx, ctx2, 'canvas', 'canvas2', 0.3, "#ffffff", "#ffffff", 8, 16, 4, 2 )
     }
-
     if(audio.currentTime >= 7 && audio.currentTime < 18){
         modifySampleEqualizerCanvas(ctx, ctx2, 'canvas', 'canvas2', 0.3, "#ffffff", "#ffffff", 16, 32, 8, 1.5 )
     }
@@ -124,6 +111,9 @@ let FrameLooper = () => {
     }
     if(audio.currentTime >= 26 && audio.currentTime <= 44){
         document.getElementById('couple-container-one').classList.add("withHalf");
+        if(document.getElementById('sep-line') != null){
+            document.getElementById('sep-line').remove();
+        }
         modifySampleEqualizerCanvas(ctx3, ctx4, 'canvas3', 'canvas4', 0.3, "#ffffff", "#ffffff", 8, 16, 4, 2)
     }
     if(audio.currentTime >= 34 && audio.currentTime <= 46){
@@ -134,14 +124,26 @@ let FrameLooper = () => {
         document.getElementById('couple-container-four').classList.add("eighthEffect");
         modifySampleEqualizerCanvas(ctx7, ctx8, 'canvas7', 'canvas8', 1,  "#ffffff", "#ffffff",2, 8, 4, 4)
     }
-    if(audio.currentTime > 42){
+    if(audio.currentTime > 42 && audio.currentTime < 92){
         modifySampleEqualizerCanvas(ctx, ctx2, 'canvas', 'canvas2', 0.3, "#0b73c5", "#0b73c5", 8, 16, 4, 2)
     }
-    if(audio.currentTime > 44){
+    if(audio.currentTime > 44 && audio.currentTime < 92){
         modifySampleEqualizerCanvas(ctx3, ctx4, 'canvas3', 'canvas4', 0.3, "#c9d547", "#c9d547", 8, 16, 4, 2)
     }
-    if(audio.currentTime >= 46){
+    if(audio.currentTime >= 46 && audio.currentTime < 92){
         modifySampleEqualizerCanvas(ctx5, ctx6, 'canvas5', 'canvas6', 1,  "#fe0000", "#fe0000",2, 8, 4, 4)
+    }
+    if(audio.currentTime >= 52 && audio.currentTime < 92){
+        document.getElementById('couple-container-five').classList.add("tenthEffect");
+        modifySampleEqualizerCanvas(ctx9, ctx10, 'canvas9', 'canvas10', 1, "#ffeb00", "#ffeb00",2, 8, 4, 4)
+    }
+    if(audio.currentTime >= 80 && audio.currentTime < 92){
+        document.getElementById('couple-container-six').classList.add("eighteenthEffect");
+        modifySampleEqualizerCanvas(ctx11, ctx12, 'canvas11', 'canvas12', 1, "#fdad03", "#fdad03",2, 8, 4, 4)
+    }
+    if(audio.currentTime >= 84 && audio.currentTime < 92){
+        document.getElementById('couple-container-seven').classList.add("twentieth");
+        modifySampleEqualizerCanvas(ctx13, ctx14, 'canvas13', 'canvas14', 1, "#eb00ff", "#eb00ff",2, 8, 4, 4)
     }
 }
 
@@ -232,6 +234,118 @@ let sixthEffect = () => {
     document.getElementById('canvas4').classList.add("sixthEffect");
 }
 
+let ninthEffect = () => {
+    document.getElementById('canvas7').classList.add("ninthEffect");
+    document.getElementById('canvas8').classList.add("ninthEffect");
+}
+
+let eleventhEffect = () => {
+    document.getElementById('canvas9').classList.add("eleventhEffect");
+    document.getElementById('canvas10').classList.add("eleventhEffect");
+}
+
+let twelfthEffect = () => {
+    document.getElementById('wave-container').classList.add("twelfthEffect");
+    //audio.pause();
+}
+
+let thirteenthEffect = () => {
+    document.getElementById('canvas').classList.add("thirteenthEffect");
+    document.getElementById('canvas2').classList.add("thirteenthEffect");
+    document.getElementById('canvas3').classList.add("thirteenthEffect");
+    document.getElementById('canvas4').classList.add("thirteenthEffect");
+    document.getElementById('wave-container').classList.add("thirteenthEffect");
+    
+}
+
+let fourteenthEffect = () => {
+    document.getElementById('canvas7').classList.add("fourteenthEffect");
+    document.getElementById('canvas8').classList.add("fourteenthEffect");
+}
+
+let fifteenthEffect = () => {
+    document.getElementById('canvas5').classList.add("fifteenthEffect");
+    document.getElementById('canvas6').classList.add("fifteenthEffect");
+    document.getElementById('canvas9').classList.add("fifteenthEffect");
+    document.getElementById('canvas10').classList.add("fifteenthEffect");
+}
+let sixteenthEffect = () => {
+    document.getElementById('canvas5').classList.add("sixteenthEffect");
+    document.getElementById('canvas6').classList.add("sixteenthEffect");
+}
+
+let seventeenthEffect = () => {
+    document.getElementById('canvas9').classList.add("seventeenthEffect");
+    document.getElementById('canvas10').classList.add("seventeenthEffect");
+}
+
+let nineteenthEffect = () => {
+    document.getElementById('canvas11').classList.add("nineteenthEffect");
+    document.getElementById('canvas12').classList.add("nineteenthEffect");
+    document.getElementById('wave-container').classList.add("nineteenthEffect");
+}
+let twentyfirstEffect = () => {
+    document.getElementById('canvas13').classList.add("twentyfirstEffect");
+    document.getElementById('canvas14').classList.add("twentyfirstEffect");
+}
+
+let twentysecondEffect = () => {
+    if(document.getElementById('canvas') != null)
+    document.getElementById('canvas').remove();
+    if(document.getElementById('canva2') != null)
+    document.getElementById('canvas2').remove();
+    if(document.getElementById('canvas3') != null)
+    document.getElementById('canvas3').remove();
+    if(document.getElementById('canvas4') != null)
+    document.getElementById('canvas4').remove();
+    if(document.getElementById('canvas5') != null)
+    document.getElementById('canvas5').remove();
+    if(document.getElementById('canvas6') != null)
+    document.getElementById('canvas6').remove();
+    if(document.getElementById('canvas9') != null)
+    document.getElementById('canvas9').remove();
+    if(document.getElementById('canvas10') != null)
+    document.getElementById('canvas10').remove();
+    if(document.getElementById('canvas11') != null)
+    document.getElementById('canvas11').remove();
+    if(document.getElementById('canvas12') != null)
+    document.getElementById('canvas12').remove();
+    if(document.getElementById('canvas13') != null)
+    document.getElementById('canvas13').remove();
+    if(document.getElementById('canvas14') != null)
+    document.getElementById('canvas14').remove();
+    document.getElementById('wave-container').classList.add("twentysecondEffect");
+}
+
+let twentythirdEffect = () => {
+    if(document.getElementById('canvas15') == null)
+    createCanvas('grid', 'canvas15');
+    let canvas15;
+    canvas15 = document.getElementById("canvas15");
+    canvas15.width = window.innerWidth;
+    canvas15.height = window.innerHeight;
+    canvas15 = document.getElementById("canvas15");
+    ctx15 = canvas.getContext("2d");
+    let widthInter = window.innerWidth / 10;
+    let heightInter = window.innerHeight / 10;
+    ctx15.strokeStyle="#fff"
+    for (let i = 0 ; i <= 10 ; i++){
+        console.log("widthInter * i ",widthInter * i)
+        console.log("heightInter * i ",heightInter * i)
+        ctx15.beginPath();
+        ctx.moveTo(heightInter * i ,0);
+        ctx15.lineTo(0, canvas15.width);
+        ctx15.stroke();
+        ctx15.closePath();
+        ctx15.beginPath();
+        ctx.moveTo(widthInter * i ,0);
+        ctx15.lineTo(widthInter * i ,canvas15.height);
+        ctx15.stroke();
+        ctx15.closePath();
+    }
+
+}
+
 generateCanvas();
 createCanvas('couple-container-two', 'canvas3');
 createCanvas('couple-container-two', 'canvas4');
@@ -239,6 +353,12 @@ createCanvas('couple-container-three', 'canvas5');
 createCanvas('couple-container-three', 'canvas6');
 createCanvas('couple-container-four', 'canvas7');
 createCanvas('couple-container-four', 'canvas8');
+createCanvas('couple-container-five', 'canvas9');
+createCanvas('couple-container-five', 'canvas10');
+createCanvas('couple-container-six', 'canvas11');
+createCanvas('couple-container-six', 'canvas12');
+createCanvas('couple-container-seven', 'canvas13');
+createCanvas('couple-container-seven', 'canvas14');
 //generateOscilloscope();
 
 document.getElementById("play").addEventListener(
@@ -252,6 +372,7 @@ document.getElementById("play").addEventListener(
             analyser.connect(context.destination);
             audio.play();
             FrameLooper();
+            //audio.currentTime = 91
             intervalId = setInterval(() => {
                 //console.log(audio.currentTime);
                 if(audio.currentTime >= 2 && audio.currentTime < 3 ){
@@ -281,7 +402,44 @@ document.getElementById("play").addEventListener(
                 else if(audio.currentTime >= 28 && audio.currentTime < 30){
                     sixthEffect();
                 }
+                else if(audio.currentTime >= 48 && audio.currentTime < 49){
+                    ninthEffect();
+                }
+                else if(audio.currentTime >52 && audio.currentTime < 53){
+                    eleventhEffect();
+                }
+                else if(audio.currentTime > 55 && audio.currentTime < 56){
+                    twelfthEffect();
+                }
+                else if(audio.currentTime > 59 && audio.currentTime < 61){
+                    thirteenthEffect();
+                }
+                else if(audio.currentTime > 63 && audio.currentTime < 65){
+                    fourteenthEffect();
+                }
+                else if(audio.currentTime > 67 && audio.currentTime < 69){
+                    fifteenthEffect();
+                }
+                else if(audio.currentTime > 71 && audio.currentTime < 73){
+                    sixteenthEffect();
+                }
+                else if(audio.currentTime > 75 && audio.currentTime < 77){
+                    seventeenthEffect();
+                }
+                else if(audio.currentTime > 79 && audio.currentTime < 81){
+                    nineteenthEffect();
+                }
+                else if(audio.currentTime > 86 && audio.currentTime < 88){
+                    twentyfirstEffect();
+                }
+                else if(audio.currentTime > 92 && audio.currentTime < 93){
+                    twentysecondEffect();
+                }
+                /* if(audio.currentTime > 96 && audio.currentTime < 98){
+                    twentythirdEffect();
+                } */
             },200);
+            twentythirdEffect();
 
         }else{
             source.connect(analyser);
@@ -289,8 +447,7 @@ document.getElementById("play").addEventListener(
             audio.play();
             FrameLooper();
         }
-    },
-    false
+    }
 );
 
 document.getElementById("pause").addEventListener(
